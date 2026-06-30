0119e3ddad16a4a2691b16cc391d133bf0012a14
osunajalejandro <osunajalejandro@gmail.com>
2026-06-30 11:01:39 -0500

    Updated views, created serializers and API endpoints

--- workspace/littlelemon/restaurant/views.py
+++ workspace/littlelemon/restaurant/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
 
-# Create your views here.
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
